a2aae5513a08d475da6741f4814bf958b58a499a
hannankm <hanankmoh@gmail.com>
2024-12-11 15:45:52 +0300

    frontend setup

--- resources/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
-import App from './App.vue';
-import router from './router';
-
-createApp(App).use(ElementPlus).use(router).mount('#app');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 </head>
 <body>
     <div id="app"></div>
